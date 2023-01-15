46f3f8687b64be7441c7e49540695f81e08594e9
Vadim <megablock20@gmail.com>
2023-01-15 20:55:10 +0300

    Работа с циклами

--- mein.js
+++ mein.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели? ");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели? ",'');
 
 const personalMovieDb = {
   count: numberOfFilms,
@@ -8,14 +8,29 @@ const personalMovieDb = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = prompt('На сколько оцените его?');
+if (personalMovieDb.count >= 1 && personalMovieDb.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDb.count >= 30) {
+  alert('Вы киноман!');
+} else {alert('Произошла ошибка');}
 
-personalMovieDb.movies [a] = b;
-personalMovieDb.movies [c] = d;      
+
+
+for(let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?'),
+        b = prompt('На сколько оцените его?');
+
+        if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+          personalMovieDb.movies [a] = b;
+        } else { i-- ;}
+        
+}
+
 
 console.log(personalMovieDb);
 
-aldjcnladvnl
+
+
+
